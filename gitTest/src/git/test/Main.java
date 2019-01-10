c938c4200215aeb2cf9b9b0770738b7b54ae5df5
user2 <user2@user2-PC>
2019-01-10 20:09:33 +0900

    sodydTJTek

--- gitTest/src/git/test/Main.java
+++ gitTest/src/git/test/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("추가한내용");
+		int age=19;
 		System.out.println("두번째 수정");
 		System.out.println("이거 충돌하니?");
 	}
