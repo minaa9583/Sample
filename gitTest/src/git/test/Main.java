caf394a7580171981d0be1c1baeed9ae92dc6a17
user2 <user2@user2-PC>
2019-01-10 20:10:26 +0900

    권민아 이름추가

--- gitTest/src/git/test/Main.java
+++ gitTest/src/git/test/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String name="권민아";
 		System.out.println("두번째 수정");
 		System.out.println("이거 충돌하니?");
 	}
